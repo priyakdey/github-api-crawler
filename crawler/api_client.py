e30a6b439d3306433e19e705754be990781133bc
Priyak Dey <35918976+priyakdey@users.noreply.github.com>
2021-10-10 04:57:28 +0530

    External Calls Logic (#13)
    * add a rest client to make extenal REST calls
    
    * add token caching and expiration logic
    
    * add logic to get all categories and data for one category

---
+++ crawler/api_client.py
@@ -0,0 +1,101 @@
+import logging
+from datetime import datetime, timedelta
+from math import ceil
+from typing import Dict, Any, List, Tuple
+
+import requests
+
+from crawler import constants as C
+from crawler.exceptions import APIException
+
+logger = logging.getLogger(__name__)
+
+
+class RestClient:
+    """Client which is capable of making Rest API calls"""
+
+    @staticmethod
+    def get(url: str, headers: Dict[str, Any] = None) -> Dict[str, Any]:
+        """Makes a get call and returns a jsonified response.
+        In case server returns non 200 code, raised APIException
+        """
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise APIException(response.status_code, "Issue with external api")  # TODO: err msg to be better
+
+
+class Token:
+    """Class is responsible to cache a Authentication token and also expiration.
+    Class has one class method get_token() which returns the cached token
+    or else makes a call if token is null or expired.
+    """
+
+    _token = None
+    _tte = None
+
+    @classmethod
+    def get_token(cls) -> str:
+        if cls._token is None or cls._is_expired():
+            # Make a call to get the token
+            logger.info("Getting a token from the server")
+            data = RestClient.get(C.GET_TOKEN_URL)
+            cls._token = data["token"]
+            cls._tte = _now() + timedelta(minutes=5)
+        return cls._token
+
+    @classmethod
+    def _is_expired(cls) -> bool:
+        """Checks if token has expired"""
+        return True if cls._tte > _now() else False
+
+
+def get_details_for_category(category: str, page: int = 1) -> Tuple[int, List[Dict[str, Any]]]:
+    """Returns the API details for a category
+
+    :param category The category for which details is requests
+    :param page Page number for which details is requested. Default value is 1
+    :returns (total count, list of api details)
+    """
+    url = C.GET_DATA_FOR_CATEGORY_URL.substitute({"page": page, "category": category})
+    token = Token.get_token()
+    headers = {"Authorization": "Bearer " + token}
+    data = RestClient.get(url, headers=headers)
+    return data["count"], data["categories"]
+
+
+def get_all_categories() -> List[str]:
+    """Returns a list of all the categories returned by the API
+
+    :returns List of categories
+    """
+    categories = []
+    data = _get_category_for_page(1)
+    _count = data["count"]
+    logger.info("Total count of categories is %s", _count)
+    categories.extend(data["categories"])
+
+    # Pagination for categories
+    start_page = 2
+    end_page = ceil(_count / 10)
+
+    for page in range(start_page, end_page + 1):
+        data = _get_category_for_page(page)
+        categories.extend(data["categories"])
+
+    return categories
+
+
+def _get_category_for_page(page: int) -> Dict[str, Any]:
+    url = C.GET_ALL_CATEGORIES_URL.substitute({"page": page})
+    token = Token.get_token()
+    headers = {"Authorization": "Bearer " + token}
+    data = RestClient.get(url, headers=headers)
+    return data
+
+
+# Cause this makes more sense than to use a third part lib to test
+def _now() -> datetime:
+    """Returns current timestamp"""
+    return datetime.now()
---
+++ tests/test_api_client.py
@@ -0,0 +1,123 @@
+from datetime import timedelta
+
+import pytest
+
+from crawler.api_client import RestClient, Token
+from crawler import api_client
+from crawler.exceptions import APIException
+
+
+class MockResponse:
+    """Class to mock server responses"""
+
+    def __init__(self, json_date, status_code):
+        self.json_data = json_date
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
+
+
+def test_get_success(mocker):
+    def mock_request_get(*args, **kwargs):
+        """Mock function to be called instead of requests.get to return a Mock reponse"""
+        return MockResponse({"key": "value"}, 200)
+
+    mocker.patch("requests.get", side_effect=mock_request_get)
+    data = RestClient.get("http://mock-url")
+    assert data == {"key": "value"}
+
+
+def test_get_exception(mocker):
+    def mock_request_get(*args, **kwargs):
+        return MockResponse(None, 404)
+
+    mocker.patch("requests.get", side_effect=mock_request_get)
+    with pytest.raises(APIException) as e:
+        RestClient.get("http://mock-url")
+    assert e.value.err_code == 404
+    assert e.value.err_msg == "Issue with external api"
+
+
+def test_get_token_if_empty_cache(mocker):
+    def mock_token(*args, **kwargs):
+        return MockResponse({"token": "some-random-token"}, 200)
+
+    mocker.patch("requests.get", side_effect=mock_token)
+    token = Token.get_token()
+    assert token == "some-random-token"
+    assert Token._tte is not None
+
+
+def test_get_token_when_not_expired(mocker):
+    def first_mock_token_from_server(*args, **kwargs):
+        return MockResponse({"token": "first-token"}, 200)
+
+    mocker.patch("requests.get", side_effect=first_mock_token_from_server)
+    assert Token.get_token() == "first-token"
+
+    # Let us mock a call 2 minutes before the expiration
+    mock_current_time = Token._tte - timedelta(minutes=-2)
+    mocker.patch("crawler.api_client._now", return_value=mock_current_time)
+
+    # a new  token should come from the server in case the actual call goes through
+    def second_mock_token_from_server(*args, **kwargs):
+        return MockResponse({"token": "second-token"}, 200)
+
+    mocker.patch("requests.get", side_effect=second_mock_token_from_server)
+    assert Token.get_token() == "first-token"
+
+
+def test_get_token_when_expired(mocker):
+    def first_mock_token_from_server(*args, **kwargs):
+        return MockResponse({"token": "first-token"}, 200)
+
+    mocker.patch("requests.get", side_effect=first_mock_token_from_server)
+    assert Token.get_token() == "first-token"
+
+    # Let us mock a call exactly 5 minutes 1 second after the expiration
+    mock_current_time = Token._tte - timedelta(minutes=5, seconds=1)
+    mocker.patch("crawler.api_client._now", return_value=mock_current_time)
+
+    # a new  token should come from the server in case the actual call goes through
+    def second_mock_token_from_server(*args, **kwargs):
+        return MockResponse({"token": "second-token"}, 200)
+
+    mocker.patch("requests.get", side_effect=second_mock_token_from_server)
+    assert Token.get_token() == "second-token"
+
+
+def test_get_details_for_category(mocker, animals_page_one, animals_page_two):
+    def mock_data(*args, **kwargs):
+        if args[0] == "https://public-apis-api.herokuapp.com/api/v1/apis/entry?page=1&category=Animals":
+            return MockResponse(animals_page_one, 200)
+        elif args[0] == "https://public-apis-api.herokuapp.com/api/v1/apis/entry?page=2&category=Animals":
+            return MockResponse(animals_page_two, 200)
+
+    mocker.patch("requests.get", side_effect=mock_data)
+    mocker.patch("crawler.api_client.Token.get_token", return_value="some-random-token")
+
+    # Test for page 1
+    count, data = api_client.get_details_for_category("Animals")
+    assert count == 13
+    assert len(data) == 10
+
+    # Test for page 3
+    count, data = api_client.get_details_for_category("Animals", 2)
+    assert count == 13
+    assert len(data) == 3
+
+
+def test_get_all_categories(mocker, categories_page_one, categories_page_two):
+    def mock_get_categories_from_server(*args, **kwargs):
+        if args[0] == "https://public-apis-api.herokuapp.com/api/v1/apis/categories?page=1":
+            return MockResponse(categories_page_one, 200)
+        elif args[0] == "https://public-apis-api.herokuapp.com/api/v1/apis/categories?page=2":
+            return MockResponse(categories_page_two, 200)
+
+    mocker.patch("requests.get", side_effect=mock_get_categories_from_server)
+    mocker.patch("crawler.api_client.Token.get_token", return_value="some-random-token")
+
+    mock_categories_list = categories_page_one["categories"] + categories_page_two["categories"]
+    categories = api_client.get_all_categories()
+    assert categories == mock_categories_list
