72767afa4bb962c3e0992035415e233f4ee7790e
Priyak Dey <35918976+priyakdey@users.noreply.github.com>
2021-10-12 00:08:02 +0530

    Refactoring (#20)
    * add dockerfile and docker-compose file update
    
    * refactoring the application

--- crawler/constants.py
+++ crawler/constants.py
@@ -2,7 +2,7 @@ from string import Template
 
 # DB Constants
 DB_TIMEOUT_IN_MS = 30000
-DB_CONN_STRING = "mongodb://admin:password@localhost:27017/"
+DB_CONN_STRING = "mongodb://admin:password@mongo:27017/"
 
 # URLs
 GET_TOKEN_URL = "https://public-apis-api.herokuapp.com/api/v1/auth/token"
